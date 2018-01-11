a2e8be80dd905ff9af747f7eea593708ee961af5
Bernardo Almeida <balmeida@lasige.di.fc.ul.pt>
2018-01-11 11:36:02 +0000

    adding

--- test/SpecHelper.hs
+++ test/SpecHelper.hs
@@ -1,7 +1,21 @@
 module SpecHelper
     ( module Test.Hspec
     , module Types.Types
+    , module Types.Parser
+    , readFromFile
+    , convert
     ) where
 
 import Test.Hspec
 import Types.Types
+import Data.Char
+import Types.Parser
+
+readFromFile filename = do
+  str <- readFile filename
+  -- print $ map (dropWhile isSpace) $ lines str
+  return $ map (dropWhile isSpace) $ lines str
+
+convert :: [String] -> [(String, String)]
+convert [] = []
+convert (k:v:t) = (k, v) : convert t
