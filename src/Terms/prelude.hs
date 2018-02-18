f855ab575c2a4a57aae63bc88ed28deae20e0c17
Bernardo Almeida <balmeida@lasige.di.fc.ul.pt>
2018-02-18 17:37:49 +0000

    adding

---
+++ src/Terms/prelude.hs
@@ -0,0 +1,11 @@
+-- Integer prelude
+(+) :: Int -> Int -> Int
+(-) :: Int -> Int -> Int
+(/) :: Int -> Int -> Int
+(*) :: Int -> Int -> Int
+mod :: Int -> Int -> Int
+rem :: Int -> Int -> Int
+
+not :: Bool -> Bool
+(&&) :: Bool -> Bool -> Bool
+(||) :: Bool -> Bool -> Bool
