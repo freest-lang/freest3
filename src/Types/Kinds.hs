9e5076d15669f71f694a29997b4fde4594ba11ec
Vasco T. Vasconcelos <vv@di.fc.ul.pt>
2018-04-29 11:31:40 +0100

    tuning

--- src/Types/Kinds.hs
+++ src/Types/Kinds.hs
@@ -1,4 +1,15 @@
--- TODO: header here
+{- |
+Module      :  Types
+Description :  <optional short text displayed on contents page>
+Copyright   :  (c) <Authors or Affiliations>
+License     :  <license>
+
+Maintainer  :  <email>
+Stability   :  unstable | experimental | provisional | stable | frozen
+Portability :  portable | non-portable (<reason>)
+
+<module description starting at first column>
+-}
 
 module Types.Kinds
 ( PreKind (..)
@@ -6,19 +17,31 @@ module Types.Kinds
 , Kind (..)
 ) where
 
-  
-
 data PreKind = Session | Functional  deriving (Eq, Ord, Read)
+
+instance Show PreKind where
+  show Session    = "S"
+  show Functional = "T"
 
 data Multiplicity = Un | Lin deriving (Eq, Ord, Read)
 
--- TODO: Turn into a record
+instance Show Multiplicity where
+  show Un  = "U"
+  show Lin = "L"
+
 data Kind = Kind PreKind Multiplicity deriving (Eq, Ord, Read)
 
--- newtype Kind = Kind {getPreKindMultiplicity :: (PreKind, Multiplicity)}
+instance Show Kind where
+  show (Kind p m) = show p ++ show m
+
+{- Alternative. Worth the refactoring?
+
+newtype Kind = Kind {getPreKindMultiplicity :: (PreKind, Multiplicity)}
 
 instance Show Kind where
-  show (Kind Session Lin) = "SL"
-  show (Kind Session Un) = "SU"
-  show (Kind Functional Lin) = "TL"
-  show (Kind Functional Un) = "TU" 
+  show k = show p ++ show m
+    where (p, m) = getPreKindMultiplicity k
+-}
+
+
+
