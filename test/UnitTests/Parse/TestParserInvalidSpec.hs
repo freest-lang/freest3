d1ffabe330b204c3442cef040aa7236b57f7f295
Bernardo Almeida <balmeida@lasige.di.fc.ul.pt>
2019-03-03 17:08:50 +0000

    tuning tests

--- test/UnitTests/Parse/TestParserInvalidSpec.hs
+++ test/UnitTests/Parse/TestParserInvalidSpec.hs
@@ -12,7 +12,7 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "Valid kinding tests" $ do
+  describe "Invalid parser tests" $ do
     t <- runIO $ readFromFile "test/UnitTests/Parse/TestParserTypesInvalid.txt"
     mapM_ matchInvalidTypesSpec t
 
@@ -22,48 +22,6 @@ matchInvalidTypesSpec t =
     evaluate (read t :: Type) `shouldThrow` anyException
 
 -- -- pendingWith "need to make this set of tests"
--- spec :: Spec
--- spec = do
--- --  evaluate (read"" :: Type) `shouldThrow` anyException
---   describe "Invalid Parser tests" $ do
---     it "Nested in out" $ do
---       evaluate(read "!? Int" :: Type) `shouldThrow` anyException
-
---     it "Semi Semi" $ do
---       evaluate(read "!Int;;?Int" :: Type)  `shouldThrow` anyException
-
---     it "Nested InternalChoice and ExternalChoice" $ do
---       evaluate(read "+&{Leaf:Skip,Node:!Int;?Int})" :: Type)  `shouldThrow` anyException
-
---     it "Out skip" $ do
---       evaluate (read "!Skip" :: Type) `shouldThrow` anyException
-
---     it "In skip" $ do
---       evaluate (read "?Skip" :: Type) `shouldThrow` anyException
-
---     it "Nested Lin Un Fun" $ do
---       evaluate (read "!Int ->-o !Int" :: Type) `shouldThrow` anyException
-
---     it "Empty read (no input)" $ do
---       evaluate (read "" :: Type) `shouldThrow` anyException
-
---     it "Parens in" $ do
---       evaluate (read "(?)Int" :: Type) `shouldThrow` anyException
-
---     it "Parens out" $ do
---        evaluate (read "(!)Char" :: Type) `shouldThrow` anyException
-
---     it "Parens LinFun" $ do
---       evaluate (read "Char (->) Char" :: Type) `shouldThrow` anyException
-
---     it "Parens UnFun"   $ do
---       evaluate (read "Char (->) Char" :: Type) `shouldThrow` anyException
-
---     it "In not basic (skip)" $ do
---       evaluate (read "?Skip" :: Type)  `shouldThrow` anyException
-
---     it "Out not basic (skip)" $ do
---       evaluate (read "!Skip" :: Type)  `shouldThrow` anyException
 
 -- out and in of a type that isn't basic
 
