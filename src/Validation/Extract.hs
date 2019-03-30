a9e002b1f5fb900be05bbd1e4808bec4fd7fd383
Vasco T. Vasconcelos <vmvasconcelos@ciencias.ulisboa.pt>
2019-03-30 12:16:40 +0000

    tuning extract

--- src/Validation/Extract.hs
+++ src/Validation/Extract.hs
@@ -12,7 +12,18 @@ Portability :  portable | non-portable (<reason>)
 -}
 
 {-# LANGUAGE LambdaCase, NoMonadFailDesugaring #-}
-module Validation.Extract where
+module Validation.Extract
+( extractScheme
+, extractFun
+, extractPair
+, extractBasic
+, extractOutput
+, extractInput
+, extractInChoice
+, extractExtChoiceMap
+, extractDataTypeMap
+, extractCons
+) where
 
 import           Parse.Lexer (Pos, position)
 import           Syntax.Types
@@ -24,33 +35,28 @@ import qualified Data.Map.Strict as Map
 
 -- | The Extract Functions
 
-normType :: Type -> FreestState Type
-normType t@(Basic _ _) = return t
-normType t@(Fun _ _ _ _) = return t
-normType t@(PairType _ _ _) = return t
-normType t@(Datatype _ _) = return t
-normType t@(Skip _) = return t
-normType t@(Message _ _ _) = return t
-normType t@(Choice _ _ _) = return t
-normType (Rec _ _ t) = (normType . unfold) t
-normType (Dualof _ t) = (normType . dual) t
-normType t@(Var p x) = do
-  getFromKenv (Bind p x) >>= \case
-    Just _    -> return t
-    Nothing -> do
-      Just (TypeScheme _ _ u) <- getFromTenv (KBind p x (top p))
-      return u
-normType (Semi _ t u) = do
-  nt <- normType t
-  nu <- normType u
-  return $ append nt nu 
-  
+-- The output is a type different from Rec and from Unfold. If it is a
+-- Var, then it represents a polymorphic variable.
+normalize :: Type -> Type
+  -- Session types
+normalize (Semi _ t u) = append (normalize t) (normalize u)
+normalize (Rec _ _ t) = normalize (unfold t)
+  -- Functional or session
+-- normalize t@(Var p x) = do
+--   getFromKenv (Bind p x) >>= \case
+--     Just _    -> return t
+--     Nothing -> do
+--       Just (TypeScheme _ _ u) <- getFromTenv (KBind p x (top p))
+--       return u
+  -- Type operators
+normalize (Dualof _ t) = normalize (dual t)
+  -- Functional types, Skip, Message, Choice, and Var
+normalize t = t
 
 append :: Type -> Type -> Type
 append (Skip _) t = t
 append (Semi p t u) v = Semi p t (append u v)
 append t v = Semi (position t) t v
-
 
 -- Extracts a typescheme; gives an error if it is on Ɐ ε ⇒ T form
 extractScheme :: TypeScheme -> FreestState ([KBind], Type)
@@ -61,19 +67,18 @@ extractScheme (TypeScheme _ bs t) = return (bs, t)
 
 -- Extracts a function from a type; gives an error if there isn't a function
 extractFun :: Type -> FreestState (Type, Type)
-extractFun t = do
-  normType t >>= \case
+extractFun t =
+  case normalize t of
     (Fun _ _ u v) -> return (u, v)
-    u             -> do
-      let p = position u
-      addError p ["Expecting a function type; found:", styleRed $ show u]
+    u             ->
+      let p = position u in
+      addError p ["Expecting a function type; found:", styleRed $ show u] >>
       return (Basic p UnitType, Basic p UnitType)
-
 
 -- Extracts a pair from a type; gives an error if there is no pair
 extractPair :: Type -> FreestState (Type, Type)
-extractPair t = do
-  normType t >>= \case
+extractPair t =
+  case normalize t of
     (PairType _ u v) -> return (u, v)
     u                ->
       let p = position u in
@@ -82,79 +87,56 @@ extractPair t = do
       
 -- Extracts a basic type from a general type; gives an error if it isn't a basic
 extractBasic :: Type -> FreestState BasicType
-extractBasic t = do
-  normType t >>= \case
+extractBasic t =
+  case normalize t of
     (Basic _ b) -> return b
     u           ->
       addError (position u) ["Expecting a basic type; found", styleRed $ show u] >>
       return UnitType
 
-
 -- Extracts an output type from a general type; gives an error if it isn't an output
-
 extractOutput :: Type -> FreestState (BasicType, Type)
-extractOutput t =  do
-  normType t >>= \case
-     (Message p Out b)            -> return (b, Skip p)
-     (Semi _ (Message _ Out b) u) -> return (b, u)
-     u                            ->
-       addError (position u) ["Expecting an output type; found", styleRed $ show u] >>
-       return (UnitType, Skip (position u))
-
+extractOutput = extractMessage Out "output"
 
 -- Extracts an input type from a general type; gives an error if it isn't an input
 extractInput :: Type -> FreestState (BasicType, Type)
-extractInput t =  do
-  normType t >>= \case
-     (Message p In b)            -> return (b, Skip p)
-     (Semi _ (Message _ In b) u) -> return (b, u)
-     u                           ->
-       addError (position u) ["Expecting an input type; found", styleRed $ show u] >>
+extractInput = extractMessage Out "input"
+
+extractMessage :: Polarity -> String -> Type -> FreestState (BasicType, Type)
+extractMessage pol msg t =
+  case normalize t of
+     (Message p pol b)            -> return (b, Skip p)
+     (Semi _ (Message _ pol b) u) -> return (b, u)
+     u                            ->
+       addError (position u) ["Expecting an", msg, "type; found", styleRed $ show u] >>
        return (UnitType, Skip (position u))
 
-
--- Extracts an internal choice from a type; gives an error if it
+-- Extracts an internal choice from a type; gives an error if it isn't an internal choice
 extractInChoice :: Type -> FreestState Type
-extractInChoice t = do
-  normType t >>= \case
-    c@(Choice _ Out _)   -> return c
-    (Semi _ (Choice p Out m) u) -> 
-       return $ Choice p Out (Map.map (\v -> Semi (position v) v u) m)
-    u                         -> do
+extractInChoice t =
+  case normalize t of
+    c@(Choice _ Out _)          -> return c
+    (Semi _ (Choice p Out m) u) -> return $ Choice p Out (Map.map (\v -> Semi (position v) v u) m)
+    u                           -> do
       addError (position u) ["Expecting an internal choice; found", styleRed $ show u]
       return $ Skip (position u)
-
-{- unused
--- Extracts an external choice from a type; gives an error if it 
-extractExtChoice :: Type -> FreestState Type
-extractExtChoice t = do
-  normType t >>= \case
-    c@(Choice _ In _)   -> return c
-    (Semi _ (Choice p In m) u) -> 
-       return $ Choice p In (Map.map (\v -> Semi (position v) v u) m)
-    u                         -> do
-      addError (position u) ["Expecting an external choice; found", styleRed $ show u]
-      return $ Skip (position u)
--}
 
 -- Extracts an external choice map from a type;
 -- gives an error if an external choice is not found
 extractExtChoiceMap :: Type -> FreestState TypeMap
-extractExtChoiceMap t = do
-  normType t >>= \case
+extractExtChoiceMap t =
+  case normalize t of
     (Choice _ In m)            -> return m
     (Semi _ (Choice _ In m) u) -> return $ Map.map (\v -> Semi (position v) v u) m
-    u                                -> do
+    u                          -> do
       addError (position u) ["Expecting an external choice; found", styleRed $ show u]
       return $ Map.empty
-
-
 
 -- Extracts a datatype from a type;
 -- gives an error if a datatype is not found
 extractDataTypeMap :: Type -> FreestState TypeMap
-extractDataTypeMap t = do
-  normType t >>= \case
+extractDataTypeMap t =
+  case normalize t of
     (Datatype _ m) -> return m
     u              -> do
       addError (position u) ["Expecting a datatype; found", styleRed $ show u]
@@ -169,7 +151,7 @@ extractCons p c (Choice _ _ tm) =
   case tm Map.!? b of
     Just t -> return t
     Nothing -> do
-      addError p ["Constructor", styleRed $ "'"++c++"'", "not in scope"]             
+      addError p ["Constructor", styleRed c, "not in scope"]             
       return (Basic p UnitType)
 extractCons p _ t = do
   addError p ["Expecting a choice; found", styleRed $ show t]
