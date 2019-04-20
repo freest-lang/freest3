37a7cc89d3e729b84c26bd34768aa9e81967109c
Vasco T. Vasconcelos <vmvasconcelos@ciencias.ulisboa.pt>
2019-04-20 01:31:34 +0100

    tuning

--- src/Equivalence/Norm.hs
+++ src/Equivalence/Norm.hs
@@ -1,5 +1,5 @@
 {- |
-Module      :  Types
+Module      :  Norm
 Description :  <optional short text displayed on contents page>
 Copyright   :  (c) <Authors or Affiliations>
 License     :  <license>
@@ -18,16 +18,13 @@ module Equivalence.Norm
 , sameNorm
 , norm
 , allNormed
-, normedWord
 ) where
 
 import           Syntax.TypeVariables
 import           Equivalence.Grammar
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
-import           Data.List (union)
-
--- Normalisation
+import qualified Data.List as List
 
 prune :: Productions -> Productions
 prune p = Map.map (Map.map (pruneWord p)) p
@@ -36,16 +33,19 @@ pruneWord :: Productions -> [TypeVar] -> [TypeVar]
 pruneWord p = foldr (\x ys -> if normed p x then x:ys else [x]) []
 
 normed :: Productions -> TypeVar -> Bool
-normed p x = normedWord p Set.empty [x]
+normed p x = normedWord p [x]
 
 type Visited = Set.Set TypeVar
 
-normedWord :: Productions -> Visited -> [TypeVar] -> Bool
-normedWord _ _ []     = True
-normedWord p v (x:xs) =
-  x `Set.notMember` v &&
-  any (normedWord p v') (Map.elems (transitions p (x:xs)))
-  where v' = if any (x `elem`) (Map.elems (transitions p [x])) then Set.insert x v else v
+normedWord :: Productions -> [TypeVar] -> Bool
+normedWord = normedW Set.empty
+  where
+  normedW :: Visited -> Productions -> [TypeVar] -> Bool
+  normedW _ _ []     = True
+  normedW v p (x:xs) =
+    x `Set.notMember` v &&
+   any (normedW v' p) (Map.elems (transitions p (x:xs)))
+   where v' = if any (x `elem`) (Map.elems (transitions p [x])) then Set.insert x v else v
 
 norm :: Productions -> [TypeVar] -> Int
 norm p xs = normList p [xs]
@@ -53,17 +53,13 @@ norm p xs = normList p [xs]
 normList :: Productions -> [[TypeVar]] -> Int
 normList p xss
   | [] `elem` m = 0
-  | otherwise = 1 + normList p (foldr union [] m)
+  | otherwise = 1 + normList p (foldr List.union [] m)
   where m = map (trans p) xss
 
 sameNorm :: Productions -> [TypeVar] -> [TypeVar] -> Bool
 sameNorm p xs ys =
-  not normedXs && not normedYs ||
-  normedXs && normedYs && norm p xs == norm p ys
-  where normedXs = normedWord p Set.empty xs
-        normedYs = normedWord p Set.empty ys
+  not (normedWord p xs) || not (normedWord p ys) || norm p xs == norm p ys
 
 -- Identify the existence of unnormed symbols
-
 allNormed :: Productions -> Bool
 allNormed p = all (normed p) (Map.keys p)
