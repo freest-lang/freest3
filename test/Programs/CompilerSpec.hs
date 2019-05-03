85a3a57af5aa012a830abe04b0c8cbbfe7bdb977
Bernardo Almeida <balmeida@lasige.di.fc.ul.pt>
2019-05-03 01:44:09 +0100

    errors are now a set

--- test/Programs/CompilerSpec.hs
+++ test/Programs/CompilerSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module CompilerSpec (spec) where
 
 import Compiler
@@ -8,12 +9,16 @@ import Test.Hspec
 import Test.HUnit (assertFailure, assertEqual)
 import System.FilePath
 import Control.Exception
+import GHC.IO.Handle   -- yes, it's GHC-specific
+import System.IO
 
 validTestDir curDir = curDir ++ "/test/Programs/ValidTests/"
 invalidTestDir curDir = curDir ++ "/test/Programs/InvalidTests/"
 
 main :: IO ()
-main = hspec $ spec
+main = do
+  hDuplicateTo stderr stdout
+  hspec $ spec
 
 spec :: Spec
 spec = do
@@ -36,10 +41,10 @@ testDir b dir curDir = parallel $ do
   validOrInvalid source
   where
     validOrInvalid source
-      | b = testOne ((validTestDir curDir) ++ dir ++ "/" ++ source) source
+      | b         = testOne ((validTestDir curDir) ++ dir ++ "/" ++ source) source
       | otherwise = testOneInvalid ((invalidTestDir curDir) ++ dir ++ "/" ++ source) source
     validOrInvalidDir b
-      | b = validTestDir curDir
+      | b         = validTestDir curDir
       | otherwise = invalidTestDir curDir
         
 getSource :: [String] -> String
@@ -47,63 +52,65 @@ getSource [] = ""
 getSource (x:xs)
   | takeExtension x == ".fst" = x
   | otherwise = getSource xs
+  
+
+exitProgram :: ExitCode -> IO (Bool, String)
+exitProgram ExitSuccess = return (True, "")
+exitProgram e = return (False, show e) 
 
 testOne :: String -> String -> Spec    
 testOne test filename = do
-  compiles <- runIO $ catch (compile test)
-                         (\e -> do let err = show (e :: SomeException)
-                                   return  $ (False, show e)
-                         )
-  case compiles of
-    (False, err) -> 
-      it ("Testing " ++ filename) $ do
-        assertFailure ("The compiler terminated with errors\n" ++ err)
-        return ()
-    (True, _)  -> -- runAndCheckResult test filename
-      -- Removed code gen
-      it ("Testing " ++ filename) $ do
-        assertEqual "Passed... without code gen" 1 1
-        return ()        
+  (b, err) <- runIO $
+    catches (compileFile test >> return (True, ""))
+                  [Handler (\(e :: ExitCode) -> exitProgram e),
+                   Handler (\(e :: SomeException) -> return (False, show e))]                  
 
+ -- Removed code gen
+  if b then
+    -- runAndCheckResult test filename   
+    it ("Testing " ++ filename) $ do
+      assertEqual "Passed... without code gen" 1 1
+      return ()
+  else
+    it ("Testing " ++ filename) $ do
+      assertFailure ("The compiler terminated with errors (check above)\n")
+      -- TODO: errors should appear here
+      return ()    
 
 testOneInvalid :: String -> String -> Spec    
 testOneInvalid test filename = do
-  compiles <- runIO $ catch (compile test)
-                         (\e -> do let err = show (e :: SomeException)
-                                   return  $ (False, show e)
-                         )
-  case compiles of
-    (False, err) -> 
-      it ("Testing " ++ filename) $ do
-        (assertEqual "" 1 1)
-        -- assertFailure ("The compiler terminated with errors\n" ++ err)
-        return ()
-    (True, _)  ->
-      it ("Testing " ++ filename) $ do
-        assertFailure ("It was expected an error but it's ok")
-        return ()
-
-runAndCheckResult :: String -> String -> Spec
-runAndCheckResult testFile filename = do
-  runIO $ setCurrentDirectory $ takeDirectory testFile
-  (exitcode, output, errors) <- runIO $ readProcessWithExitCode "ghc"
-                                         ["-dynamic", "-XBangPatterns", (replaceExtensions filename "hs")] ""  
-  
-  if (exitcode == ExitSuccess) then
-    do     
-      (exitcode1, output1, errors1) <- runIO $ readProcessWithExitCode ("./" ++ dropExtension filename) [] ""
-      if (exitcode1 /= ExitSuccess) then
-         it ("Testing " ++ filename) $ do
-           assertFailure errors1
-           return ()
-      else do  
-         cont <- runIO $ readFile ((takeWhile (/= '.') testFile) ++ ".expected")
-         it ("Testing " ++ filename) $ do
-           (filter (/= '\n') output1) `shouldBe` (filter (/= '\n') cont)
-  else
+  (b, err) <- runIO $ catch (compileFile test >> return (True,""))
+                        (\e -> return  $ (False, show (e :: SomeException)))
+  if b then
     it ("Testing " ++ filename) $ do
-      assertFailure errors
+      assertFailure ("It was expected an error but it's ok")
       return ()
+  else
+   it ("Testing " ++ filename) $ do
+        (assertEqual "OK. Passed!" 1 1)
+
+-- Review - code gen
+-- runAndCheckResult :: String -> String -> Spec
+-- runAndCheckResult testFile filename = do
+--   runIO $ setCurrentDirectory $ takeDirectory testFile
+--   (exitcode, output, errors) <- runIO $ readProcessWithExitCode "ghc"
+--                                          ["-dynamic", "-XBangPatterns", (replaceExtensions filename "hs")] ""  
+  
+--   if (exitcode == ExitSuccess) then
+--     do     
+--       (exitcode1, output1, errors1) <- runIO $ readProcessWithExitCode ("./" ++ dropExtension filename) [] ""
+--       if (exitcode1 /= ExitSuccess) then
+--          it ("Testing " ++ filename) $ do
+--            assertFailure errors1
+--            return ()
+--       else do  
+--          cont <- runIO $ readFile ((takeWhile (/= '.') testFile) ++ ".expected")
+--          it ("Testing " ++ filename) $ do
+--            (filter (/= '\n') output1) `shouldBe` (filter (/= '\n') cont)
+--   else
+--     it ("Testing " ++ filename) $ do
+--       assertFailure errors
+--       return ()
 
   
 
