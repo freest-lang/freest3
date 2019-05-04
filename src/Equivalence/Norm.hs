2a380b63f78cef18be5cb1dc5fad12f66e9aa0c4
Vasco T. Vasconcelos <vmvasconcelos@ciencias.ulisboa.pt>
2019-05-04 16:44:43 +0100

    new termination condition for norm

--- src/Equivalence/Norm.hs
+++ src/Equivalence/Norm.hs
@@ -21,53 +21,35 @@ import           Equivalence.Grammar
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 import           Data.Maybe
+import           Data.List
 
 normed :: Productions -> TypeVar -> Bool
-normed p x = isJust $ nor p [x]
--- normed p x = normedWord p [x]
-
--- normedWord :: Productions -> [TypeVar] -> Bool
--- normedWord = normedW Set.empty
---   where
---   normedW :: Visited -> Productions -> [TypeVar] -> Bool
---   normedW _ _ []     = True
---   normedW v p (x:xs) =
---     x `Set.notMember` v &&
---    any (normedW v' p) (Map.elems (transitions (x:xs) p))
---    where v' = if any (x `elem`) (Map.elems (transitions x p)) then Set.insert x v else v
+normed p x = isJust $ maybeNorm p [x]
 
 norm :: Productions -> [TypeVar] -> Int
-norm p xs = fromJust $ nor p xs
--- norm p xs = normList p [xs]
-
--- normList :: Productions -> [[TypeVar]] -> Int
--- normList p xss
---   | [] `elem` m = 0
---   | otherwise = 1 + normList p (foldr List.union [] m)
---   where m = map (trans p) xss
+norm p xs = fromJust $ maybeNorm p xs
 
 sameNorm :: Productions -> [TypeVar] -> [TypeVar] -> Bool
-sameNorm p xs ys = nor p xs == nor p ys
-  -- not (normedWord p xs) || not (normedWord p ys) || norm p xs == norm p ys
+sameNorm p xs ys = maybeNorm p xs == maybeNorm p ys
 
 -- Identify the existence of unnormed symbols
 allNormed :: Productions -> Bool
 allNormed p = all (normed p) (Map.keys p)
 
-type Visited = Set.Set TypeVar
+type Visited = Set.Set [TypeVar]
 
-nor :: Productions -> [TypeVar] -> Maybe Int
-nor p = norm' Set.empty
+maybeNorm :: Productions -> [TypeVar] -> Maybe Int
+maybeNorm p = norm Set.empty
   where
-  norm' :: Visited -> [TypeVar] -> Maybe Int
-  norm' _ [] = Just 0
-  norm' v xs
-    | (head xs) `Set.member` v &&
-        not (Map.null (Map.filter (not . null) (transitions (head xs) p))) = Nothing
-    | otherwise                = fmap (+1) (Map.foldr min' Nothing (norms v xs))
+  norm :: Visited -> [TypeVar] -> Maybe Int
+  norm _ [] = Just 0
+  norm v xs
+    | not $ Set.null $ Set.filter (flip isSubsequenceOf xs) v = Nothing
+    | otherwise = fmap (+1) (Map.foldr (compose min) Nothing (norms v xs))
   norms :: Visited -> [TypeVar] ->  Map.Map Label (Maybe Int)
-  norms v xs = Map.map (norm' (Set.insert (head xs) v)) (transitions xs p)
-  min' :: Maybe Int -> Maybe Int -> Maybe Int
-  min' Nothing  m        = m
-  min' m        Nothing  = m
-  min' (Just n) (Just k) = Just (min n k)
+  norms v xs = Map.map (norm (Set.insert xs v)) (transitions xs p)
+
+compose :: (a -> a -> a) -> Maybe a -> Maybe a -> Maybe a
+compose _ Nothing  m        = m
+compose _ m        Nothing  = m
+compose f (Just x) (Just y) = Just (f x y)
