1b9a45d8359cf811cad0be2ff8d28238e9ead0ef
Bernardo Almeida <balmeida@lasige.di.fc.ul.pt>
2019-06-24 00:51:32 +0100

    updating script

--- testSuiteProf.sh
+++ testSuiteProf.sh
@@ -7,6 +7,7 @@ echo "Number of runs: $N" >> testSuiteProf.prof
 for i in $( seq 1 $N )
 do
    echo "---------------------------------" >> testSuiteProf.prof
-   cabal test testEquiv
+   #cabal test testEquiv
+   cabal test bissimEquiv
    cat testEquiv.prof | grep "total" >> testSuiteProf.prof
 done
