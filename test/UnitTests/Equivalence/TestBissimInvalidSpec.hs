0a826b2e243940482120ea59d6514bd6cb4384cd
Bernardo Almeida <balmeida@lasige.di.fc.ul.pt>
2019-06-24 13:55:50 +0100

    adding some invalid bissim tests

--- test/UnitTests/Equivalence/TestBissimInvalidSpec.hs
+++ test/UnitTests/Equivalence/TestBissimInvalidSpec.hs
@@ -4,7 +4,7 @@ import           Syntax.Types
 import           Syntax.Kinds
 import           Syntax.TypeVariables
 import           Syntax.Base
-import           Equivalence.Equivalence
+import           Equivalence.Bisimulation (bisimilar)
 import           Validation.Rename
 import           Utils.FreestState
 import qualified Data.Map.Strict as Map
@@ -16,7 +16,7 @@ matchInvalidSpec :: [String] -> Spec
 matchInvalidSpec [t, u] =
   it (t ++ " ~ " ++  u) (
       {-# SCC "BISIM_TEST_CALL" #-}
-      equivalent Map.empty Map.empty t' u' `shouldBe` False)
+      bisimilar Map.empty t' u' `shouldBe` False)
     where
       [t', u'] = renameTypes [read t, read u]
       -- readKenv :: String -> KindEnv
