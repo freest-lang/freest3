5147236dcbb64bc7d96d117b8e9decb1f2a886bd
Alcides Fonseca <me@alcidesfonseca.com>
2019-11-01 13:46:04 +0000

    Running script now compiles once only

--- runTACAS.sh
+++ runTACAS.sh
@@ -2,16 +2,18 @@
 
 rm -rf run_*.log
 
+ghc --make -isrc -itest/ test/TACAS2020/TestTACAS.hs
+
 for (( t = 0; t < 1000; t++ )); do
    for (( i = 0; i < 8; i++ )); do
-      runhaskell -isrc -itest/ test/TACAS2020/TestTACAS.hs $t $i 1000000 True >> run_positives_$i.log &
+      ./test/TACAS2020/TestTACAS $t $i 1000000 True >> run_positives_$i.log &
    done
    wait
 done
 
 for (( t = 0; t < 1000; t++ )); do
    for (( i = 0; i < 8; i++ )); do
-      runhaskell -isrc -itest/ test/TACAS2020/TestTACASN.hs $t $i 1000000 False >> run_negatives_$i.log &
+      ./test/TACAS2020/TestTACAS $t $i 1000000 False >> run_negatives_$i.log &
    done
    wait
 done
