21dbf99b75cb548b11e6c51a9723d1c1bd328e04
Bernardo Almeida <balmeida@lasige.di.fc.ul.pt>
2019-12-12 00:57:17 +0000

    fixing tests (LTS: Label is now a String)

--- test/UnitTests/Equivalence/TestGrammarValidSpec.hs
+++ test/UnitTests/Equivalence/TestGrammarValidSpec.hs
@@ -1,18 +1,19 @@
 module Equivalence.TestGrammarValidSpec (spec) where
 
-import           Syntax.Types
+import           Control.Monad (foldM_)
+import           Data.List.Split as L
+import qualified Data.Map.Strict as Map
+import           Equivalence.Bisimulation (bisimilarGrammar)
+import           Equivalence.Grammar
+import           Parse.GrammarParser
+import           SpecHelper
+import           Syntax.Base
 import           Syntax.Kinds
 import           Syntax.TypeVariables
-import           Syntax.Base
-import           Equivalence.Bisimulation (bisimilarGrammar)
-import           Equivalence.GrammarParser
-import           Equivalence.Grammar
-import           Validation.Rename
+import           Syntax.Types
 import           Utils.FreestState
-import qualified Data.Map.Strict as Map
-import           SpecHelper
-import           Control.Monad (foldM_)
-
+import           Validation.Rename
+import qualified Control.Monad as M
 -- Note that the tests cases should be kinded!
 
 matchValidSpec :: Int -> Grammar -> Spec
@@ -23,8 +24,10 @@ matchValidSpec i g =
 spec :: Spec
 spec = do
   t <- runIO $ readFromFile "test/UnitTests/Equivalence/TestGrammarValid.txt"
-  let gs = parseGrammars (unlines t)
-  describe "Valid Bissim Test" $ foldM_ (\i g -> matchValidSpec i g >> return (i+1)) 1 gs
+--  let gs = parseGrammars (unlines t)
+  gs <- runIO $ M.sequence $ map parseGrammar (L.splitOn "|" (unlines t))  
+  describe "Valid Bissim Test" $
+    foldM_ (\i g -> matchValidSpec i g >> return (i+1)) 1 gs
 
 
 main :: IO ()
