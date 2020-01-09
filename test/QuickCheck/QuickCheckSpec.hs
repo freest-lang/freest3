2fef5ef0da9ed67b2194784c9c672069ec50bc7c
Vasco T. Vasconcelos <vmvasconcelos@ciencias.ulisboa.pt>
2020-01-09 23:14:31 +0000

    one TestEquivalenceValid test that hangs

--- test/QuickCheck/QuickCheckSpec.hs
+++ test/QuickCheck/QuickCheckSpec.hs
@@ -9,7 +9,8 @@ spec :: Spec
 spec =
   describe "QuickCheck" $ do
     prop "prop_bisimilar" $
-      verboseCheckWith stdArgs {maxSuccess = 27100, replay = Just (mkQCGen 1095646480, 0)} prop_bisimilar -- prop_distribution
+      verboseCheckWith stdArgs {maxSuccess = 108375, replay = Just (mkQCGen 1095646480, 0)} prop_distribution -- prop_bisimilar
+      -- verboseCheckWith stdArgs {maxSuccess = 27100, replay = Just (mkQCGen 1095646480, 0)} prop_bisimilar -- prop_distribution
       -- verboseCheckWith stdArgs {maxSuccess = 10000} prop_bisimilar
 
 main = hspec spec
