8b28028ad42eccc207767045d61b3f1b4bfde144
Bernardo Almeida <balmeida@lasige.di.fc.ul.pt>
2020-01-12 17:36:48 +0000

    tuning tests

--- test/UnitTests/Validation/TestTypesInvalidSpec.hs
+++ test/UnitTests/Validation/TestTypesInvalidSpec.hs
@@ -6,6 +6,8 @@ import           Utils.FreestState
 import           SpecHelper
 import           Control.Monad.State
 import qualified Data.Map.Strict as Map
+import           Syntax.Base (defaultPos)
+-- import           Data.Either
 
 spec :: Spec
 spec = do
@@ -16,13 +18,27 @@ spec = do
 matchInvalidKindingSpec :: String -> Spec
 matchInvalidKindingSpec str =
   it str $ do
-    (read str `isWellFormed` Map.empty) `shouldBe` False
+    (str `isWellFormed` Map.empty) `shouldBe` False
 
-isWellFormed :: Type -> KindEnv -> Bool
-isWellFormed t k =
-  let s = initialState "" in
-  let s1 = execState (synthetise Map.empty t) s in -- (s {kindEnv=k}) in
-    null (errors s1)
+-- either :: (a -> c) -> (b -> c) -> Either a b -> c
+-- If the value is Left a, apply the first function to a;
+-- if it is Right b, apply the second function to b.
+
+isWellFormed :: String -> KindEnv -> Bool
+isWellFormed str _ =
+  let t = parseType str in
+    either synthetiseK (\_ -> False) t
+  where
+    synthetiseK t =
+      let s  = initialState ""
+          s1 = execState (synthetise Map.empty t) s in
+          null (errors s1)
+
+-- isWellFormed :: Type -> KindEnv -> Bool
+-- isWellFormed t k =
+--   let s = initialState "" in
+--   let s1 = execState (synthetise Map.empty t) s in -- (s {kindEnv=k}) in
+--     null (errors s1)
 
 -- INVALID:
 -- forall alpha . (rec Tree . &{Leaf:Skip, Node:?Int;Tree;Tree}) -> (rec TreeChannel . +{Leaf:Skip, Node:!Int;TreeChannel;TreeChannel});alpha->alpha
