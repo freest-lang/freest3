645b17fb973eb1f311cd4d5a813e0b81996c0329
Bernardo Almeida <balmeida@lasige.di.fc.ul.pt>
2020-06-19 02:36:51 +0100

    removing unnecessary packages; adding release script

---
+++ scripts/release.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+# Create folder on tmp and copy files
+
+version=`cat ../FreeST/package.yaml | grep -e "version:" | sed -E 's/version\W+//g'`
+path=/tmp/freest-$version/
+
+mkdir $path
+# mkdir $path/FreeST/
+cp -r ../FreeST/src $path
+rm $path/src/Parse/Parser.hs $path/src/Parse/Lexer.hs
+
+resolverVersion=`cat ../stack.yaml | grep -e "^resolver:" | sed -E 's/resolver\W+//g'`
+
+# Create stack.yaml file
+
+echo "# FreeST stack file
+# Authors: Bernardo Almeida, Vasco T. Vasconcelos, Andreia Mordido
+
+# Resolver to choose a 'specific' stackage snapshot or a compiler version.
+resolver: $resolverVersion
+
+# packages to build
+packages:
+- ." > $path/stack.yaml
+
+# Create package.yaml file
+
+package=$path/package.yaml
+
+name=`cat ../FreeST/package.yaml | grep -e "name:"`
+license=`cat ../FreeST/package.yaml | grep -e "license:"`
+author=`cat ../FreeST/package.yaml | grep -e "author:"`
+copyright=`cat ../FreeST/package.yaml | grep -e "copyright:"`
+
+echo $name >> $package
+echo "version: "$version >> $package
+echo $license >> $package
+echo $author >> $package
+echo $copyright >> $package
+echo "" >> $package
+
+echo "$(awk '/^dependencies:/,/^$/' ../FreeST/package.yaml)" >> $package
+echo "" >> $package
+echo "$(awk '/^executables:/,/^$/' ../FreeST/package.yaml)" >> $package
+
+
+# Create README.md
+
+echo "\`\`\`
+  ______              _____ _______ 
+ |  ____|            / ____|__   __|
+ | |__ _ __ ___  ___| (___    | |
+ |  __| '__/ _ \/ _ \\___ \   | |
+ | |  | | |  __/  __/____) |  | |
+ |_|  |_|  \___|\___|_____/   |_|
+\`\`\`
+
+# Install stack
+- For Un*x-like operating systems:
+\`\`\`
+    $ curl -sSL https://get.haskellstack.org/ | sh
+\`\`\`
+
+- For windows download the installer available at: 
+  
+  [Get stack for windows](https://get.haskellstack.org/stable/windows-x86_64-installer.exe)
+
+For more information about stack please visit [https://docs.haskellstack.org/en/stable/README/](https://docs.haskellstack.org/en/stable/README/)
+
+# Install FreeST
+\`\`\`
+stack install
+\`\`\`
+
+If you get the following warning:
+\`\`\`
+Warning: Installation path /home/.../.local/bin
+         not found on the PATH environment variable.
+\`\`\`
+
+add this path to your PATH environment variable: 
+\`\`\`
+export PATH=/home/.../.local/bin/:$PATH
+\`\`\`
+
+this will only work for the current terminal session. If you want to keep this, add the previous line to your ~/.bashrc file.
+
+
+# Run FreeST (program example.fst)
+\`\`\`
+freest example.fst
+\`\`\`
+
+# Tutorial and some examples
+
+Please visit [RSS Tryit](http://rss.di.fc.ul.pt/tryit/FreeST)
+
+" > $path/README.md
+
+scriptDir=`pwd`
+pushd /tmp/
+
+tar -cf FreeST-$version.tar freest-$version/
+rm -rf $path
+mv FreeST-$version.tar $scriptDir
+
+# make tar
+# remove dir
