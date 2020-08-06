49248e7df40a39072f7352937069a0c2937cab74
Vasco T. Vasconcelos <vmvasconcelos@ciencias.ulisboa.pt>
2020-08-06 10:32:06 +0100

    An example with a non-guarded polymorphic var

--- FreeST/src/Validation/Terminated.hs
+++ FreeST/src/Validation/Terminated.hs
@@ -1,5 +1,5 @@
 {-|
-Module      :  Contractive
+Module      :  Validation.Terminated
 Description :  <optional short text displayed on contents page>
 Copyright   :  (c) <Authors or Affiliations>
 License     :  <license>
@@ -18,21 +18,9 @@ where
 
 import           Syntax.Types
 
--- As in the ICFP'16 paper, except that no substitution is applied to Rec types
+-- As in the ICFP'16 paper, except that Rec types are not terminated. Any type of the form 'rec a.T' with T terminated is not contractive, hence not well formed.
 terminated :: Type -> Bool
 terminated (Skip _)     = True
 terminated (Semi _ t u) = terminated t && terminated u
-terminated (Rec _ _ t)  = terminated t
+-- terminated (Rec _ _ t)  = terminated t
 terminated _            = False
-
-{- A "better" terminated predicate should be such that only free variables *of kind SU* are terminated
-
-terminated :: KindEnv -> Type ->  Bool
-terminated kEnv t = term t
-  where
-  term (Skip _)      = True
-  term (Semi _ s t)  = term s && term t
-  term (Rec _ _ t)   = term t
-  term (TypeVar _ x) = isUn (kEnv Map.! x)
-  term _             = False
--}
