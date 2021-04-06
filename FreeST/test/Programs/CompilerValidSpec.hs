b5dda25d596ffd2cc2a0099d8c5a1be09e4a322f
Bernardo Almeida <balmeida@lasige.di.fc.ul.pt>
2021-04-06 14:01:15 +0100

    fixing test infrastructure

--- FreeST/test/Programs/CompilerValidSpec.hs
+++ FreeST/test/Programs/CompilerValidSpec.hs
@@ -1,19 +1,24 @@
 {-# LANGUAGE ScopedTypeVariables, LambdaCase #-}
-module CompilerValidSpec (spec) where
+module CompilerValidSpec
+  ( spec
+  )
+where
 
-import Control.Exception
-import Control.Monad (void)
-import FreeST (checkAndRun)
-import SpecUtils
-import System.Directory
-import System.Exit
-import System.IO (stdout, stderr)
-import System.IO.Silently (hCapture)
-import System.Timeout
-import Test.HUnit (assertFailure)
-import Test.Hspec
-
-import System.FilePath -- ((</>))
+import           Control.Exception
+import           Control.Monad                  ( void )
+import           FreeST                         ( checkAndRun )
+import           SpecUtils
+import           System.Directory
+import           System.Exit
+import           System.IO                      ( stdout
+                                                , stderr
+                                                )
+import           System.IO.Silently             ( hCapture )
+import           System.Timeout
+import           Test.HUnit                     ( assertFailure )
+import           Test.Hspec
+import           Util.FreestState
+import           System.FilePath -- ((</>))
 
 data TestResult = Timeout | Passed | Failed
 
@@ -21,7 +26,7 @@ baseTestDir :: String
 baseTestDir = "/test/Programs/ValidTests/"
 
 spec :: Spec
-spec = specTest "Valid Tests" baseTestDir testValid 
+spec = specTest "Valid Tests" baseTestDir testValid
 
 testValid :: String -> String -> Spec
 testValid baseDir testingDir = do
@@ -32,21 +37,22 @@ testValid baseDir testingDir = do
   checkResult testResult f
 
 testOne :: FilePath -> IO (String, TestResult)
-testOne file =
- hCapture [stdout, stderr] $
-   catches runTest
-      [Handler (\(e :: ExitCode)      -> exitProgram e),
-       Handler (\(_ :: SomeException) -> pure Failed)]
-  where
-    exitProgram :: ExitCode -> IO TestResult
-    exitProgram ExitSuccess = pure Passed -- return True
-    exitProgram _           = pure Failed   -- return False
+testOne file = hCapture [stdout, stderr] $ catches
+  runTest
+  [ Handler (\(e :: ExitCode) -> exitProgram e)
+  , Handler (\(_ :: SomeException) -> pure Failed)
+  ]
+ where
+  exitProgram :: ExitCode -> IO TestResult
+  exitProgram ExitSuccess = pure Passed -- return True
+  exitProgram _           = pure Failed   -- return False
 
-    runTest :: IO TestResult
-    runTest =
-      timeout timeInMicro (checkAndRun file) >>= \case
-        Just _  -> pure Passed
-        Nothing -> pure Timeout
+  runTest :: IO TestResult
+  runTest =
+    timeout timeInMicro (checkAndRun defaultOpts { runFilePath = Just file })
+      >>= \case
+            Just _  -> pure Passed
+            Nothing -> pure Timeout
 
 -- n microseconds (1/10^6 seconds).
 timeInMicro :: Int
@@ -54,24 +60,28 @@ timeInMicro = 6 * 1000000
 
 checkResult :: (String, TestResult) -> FilePath -> Spec
 checkResult (res, Passed) file = checkAgainstExpected file res
-checkResult (res, Failed) file = 
-  it ("Testing " ++ takeFileName file) $
-    void $ assertFailure res
-checkResult (_, Timeout) file  = checkAgainstExpected file "<divergent>"
+checkResult (res, Failed) file =
+  it ("Testing " ++ takeFileName file) $ void $ assertFailure res
+checkResult (_, Timeout) file = checkAgainstExpected file "<divergent>"
 
 checkAgainstExpected :: FilePath -> String -> Spec
 checkAgainstExpected file res = do
   let expFile = file -<.> "expected"
   runIO (safeRead expFile) >>= \case
     Just s ->
-      it ("Testing " ++ takeFileName file) $
-        filter (/= '\n') res `shouldBe` filter (/= '\n') s
+      it ("Testing " ++ takeFileName file)
+        $          filter (/= '\n') res
+        `shouldBe` filter (/= '\n') s
     Nothing ->
-      it ("Testing " ++ takeFileName file) $
-        void $ assertFailure $ "File " ++ expFile ++ " not found"        
-  where
-    safeRead :: FilePath -> IO (Maybe String)
-    safeRead f = do
-      b <- doesFileExist f
-      if b then fmap Just (readFile f) else return Nothing
-        
+      it ("Testing " ++ takeFileName file)
+        $  void
+        $  assertFailure
+        $  "File "
+        ++ expFile
+        ++ " not found"
+ where
+  safeRead :: FilePath -> IO (Maybe String)
+  safeRead f = do
+    b <- doesFileExist f
+    if b then fmap Just (readFile f) else return Nothing
+
