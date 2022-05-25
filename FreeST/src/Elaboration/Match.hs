7996ebc0f3992cbc82adaeacdc8b6cc9c39cf49c
jotafilip <joao.roq@hotmail.com>
2022-05-26 00:54:24 +0100

    Case: var gen for invisible vars

--- FreeST/src/Elaboration/Match.hs
+++ FreeST/src/Elaboration/Match.hs
@@ -1,5 +1,6 @@
 module Elaboration.Match
   ( matchFun
+    match
   )
 where
 
@@ -9,35 +10,52 @@ import qualified Data.Map.Strict   as Map
 
 import           Util.FreestState
 
+var :: Variable
+var = Varibale undefined id
+  where id = "#u"++(show 0) -- TODO change 0
+
+vars :: Int -> [Variable]
+vars n = replicate n var
+
+-- just to remember the format
+-- cases
 -- type FieldMap  = Map.Map Variable ([Variable], Exp)
 -- type FieldMapP = Map.Map Variable ([Pattern], Exp)
+-- functions
+-- type ParseEnv  = Map.Map Variable ([Variable], Exp)
+-- type ParseEnvP = Map.Map Variable [([Pattern], Exp)]
 
---                 fun args       patterns, exp     otherwise
-type Matching = ( [Variable] , [([Pattern], Exp)] , Exp       )
+--                fun args       patterns, exp     otherwise
+data Match = M ( [Variable] , [([Pattern], Exp)] , Match    )
+           | ERROR
 
--- TODO
 matchFun :: ParseEnvP -> ParseEnv
-matchFun pep = Map.map (match) pep
+matchFun pep = Map.map (match vs) pep
+  where vs = vars n
+-- TODO
+-- transformar uma funcção num case
+-- traduzir o case
+-- adicionar o case na funcao
 
-match :: [([Pattern], Exp)] -> ([Variable], Exp)
-match ps = (undefined, undefined)
+match :: [Variable] -> [([Pattern], Exp)] -> ([Variable], Exp)
+match vs ps = undefined
 -- TODO
 -- criar len [Pattern] variaveis, porque sao os argumentos
--- transformar [([Pattern],Exp)] into Matching
+-- transformar [([Pattern],Exp)] into Match
 -- identificar a rule correta
 -- chamar a rule sobre o estado
 -- recursivo até chegar à rule empty?
 
 -- > ainda tenho de descobrir como fazer os cases
 
-ruleEmpty :: Matching -> Matching
+ruleEmpty :: Match -> Match
 ruleEmpty x = x
 
-ruleVar :: Matching -> Matching
+ruleVar :: Match -> Match
 ruleVar x = x
 
-ruleCon :: Matching -> Matching
+ruleCon :: Match -> Match
 ruleCon x = x
 
-ruleMix :: Matching -> Matching
+ruleMix :: Match -> Match
 ruleMix x = x
