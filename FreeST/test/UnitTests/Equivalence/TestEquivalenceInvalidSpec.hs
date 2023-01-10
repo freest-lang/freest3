a3da926385339e2febf79b22d42b6b70fdfed139
Vasco T. Vasconcelos <vmvasconcelos@ciencias.ulisboa.pt>
2023-01-10 00:55:41 +0000

    adapting tests: equivalence -> bisimilar

--- FreeST/test/UnitTests/Equivalence/TestEquivalenceInvalidSpec.hs
+++ FreeST/test/UnitTests/Equivalence/TestEquivalenceInvalidSpec.hs
@@ -3,18 +3,17 @@ module Equivalence.TestEquivalenceInvalidSpec
   )
 where
 
-import           Equivalence.Equivalence        ( equivalent )
+import           Bisimulation.Bisimulation        ( bisimilar )
 import           Validation.Rename
 import           Syntax.Type
 import           Util.FreestState
 import           SpecUtils
-import qualified Data.Map.Strict               as Map
 import           Control.Monad.State
 
 matchInvalidSpec :: [String] -> Spec
 matchInvalidSpec [a, b] = it
   (a ++ " ~/~ " ++ b)
-  (equivalent Map.empty a' b' `shouldBe` False)
+  (bisimilar a' b' `shouldBe` False)
  where
   [a', b'] = renameTypes [read a, read b]
 
