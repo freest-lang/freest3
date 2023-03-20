2d045dd5937150657711b69187561da4f2a72624
Vasco T. Vasconcelos <vmvasconcelos@ciencias.ulisboa.pt>
2023-03-20 20:23:47 +0000

    adding support for dualof in Equivalence.TestEquivalenceValid

--- FreeST/test/UnitTests/Equivalence/TestEquivalenceValidSpec.hs
+++ FreeST/test/UnitTests/Equivalence/TestEquivalenceValidSpec.hs
@@ -6,12 +6,13 @@ where
 import           Syntax.Kind                   as K
 import           Bisimulation.Bisimulation      ( bisimilar )
 import           Validation.Rename
+import           Elaboration.ResolveDuality
 import           Validation.Kinding             ( synthetise )
 import           SpecUtils
 import           Util.FreestState               ( initialState
                                                 , errors
                                                 )
-import           Control.Monad.State            ( execState )
+import           Control.Monad.State            ( execState, evalState )
 
 matchValidSpec :: [String] -> Spec
 matchValidSpec [k, t, u] |
@@ -20,8 +21,11 @@ matchValidSpec [k, t, u] |
     (k ++ "  |-  " ++ t ++ " ~ " ++ u)
     (bisimilar t' u' `shouldBe` True)
   where
-    [t', u'] = renameTypes [read t, read u]
-    kEnv     = readKenv k
+    kEnv = readKenv k
+    [t', u'] = renameTypes [resolveDuals $ read t, resolveDuals $ read u]
+
+resolveDuals :: Type -> Type
+resolveDuals t = evalState (resolve t) initialState
 
 wellFormed :: K.KindEnv -> Type -> Bool
 wellFormed kEnv t = null $ errors $ execState (synthetise kEnv t) initialState
