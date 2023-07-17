5d6a64e1063bc32a66e3e8381fdd2ef49e63a2ee
Diogo Barros <diogofpbarros@gmail.com>
2023-07-17 12:10:00 +0100

    keepSrc when creating new vars

--- FreeST/src/Syntax/Base.hs
+++ FreeST/src/Syntax/Base.hs
@@ -98,7 +98,7 @@ intern (Variable _ x) = x
 
 -- Making a variable from a string, type or program
 mkVar :: Span -> String -> Variable
-mkVar = Variable
+mkVar s name = Variable s{source=name} name
 
 isWild :: Variable -> Bool
 isWild (Variable _ x) = x == "_"
@@ -106,7 +106,7 @@ isWild (Variable _ x) = x == "_"
 -- Making a new variable from a given variable. The variable is
 -- unique up to the point where the integer is
 mkNewVar :: Int -> Variable -> Variable
-mkNewVar next (Variable p str) = Variable p (show next ++ '#' : str)
+mkNewVar next (Variable p str) = Variable p{source=str} (show next ++ '#' : str)
 
 -- Bind: (λ x:t -> e), (∀ a:k . t) or (Λ a:k => e) 
 data Bind a b = Bind {bSpan :: Span, var :: Variable, binder :: a, body :: b}
