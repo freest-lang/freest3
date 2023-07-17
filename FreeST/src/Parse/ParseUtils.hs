e3787200317e4911a9a3da48c0579256f302f869
Diogo Barros <diogofpbarros@gmail.com>
2023-07-17 12:15:51 +0100

    all parse time sources must be empty

--- FreeST/src/Parse/ParseUtils.hs
+++ FreeST/src/Parse/ParseUtils.hs
@@ -37,27 +37,27 @@ type FreestStateT = StateT FreestS (Either ErrorType)
 
 mkSpan :: Located a => a -> FreestStateT Span
 mkSpan a = do
-  let (Span p1 p2 src _) = getSpan a
+  let (Span p1 p2 _ _) = getSpan a
   f <- getFileName
-  maybe (Span p1 p2 src f) (Span p1 p2 src) <$> getModuleName
+  maybe (Span p1 p2 "" f) (Span p1 p2 "") <$> getModuleName
 
 mkSpanSpan :: (Located a, Located b) => a -> b -> FreestStateT Span
 mkSpanSpan a b = do
-  let (Span p1 _ src _) = getSpan a
+  let (Span p1 _ _ _) = getSpan a
   let (Span _ p2 _   _) = getSpan b
   f <- getFileName
-  maybe (Span p1 p2 src f) (Span p1 p2 src) <$> getModuleName
+  maybe (Span p1 p2 "" f) (Span p1 p2 "") <$> getModuleName
 
 mkSpanFromSpan :: Located a => Span -> a -> FreestStateT Span
 mkSpanFromSpan (Span p1 _ _ _) a = do
-  let (Span _ p2 src _) = getSpan a
+  let (Span _ p2 _ _) = getSpan a
   f <- getFileName
-  maybe (Span p1 p2 src f) (Span p1 p2 src) <$> getModuleName
+  maybe (Span p1 p2 "" f) (Span p1 p2 "") <$> getModuleName
 
 liftModToSpan :: Span -> FreestStateT Span
-liftModToSpan (Span p1 p2 src _) = do
+liftModToSpan (Span p1 p2 _ _) = do
   f <- getFileName
-  maybe (Span p1 p2 src f) (Span p1 p2 src) <$> getModuleName
+  maybe (Span p1 p2 "" f) (Span p1 p2 "") <$> getModuleName
 
 -- Parse errors
 
@@ -132,7 +132,7 @@ checkDupVarPats' ((E.PatVar  v)   :xs) vs = do
 
 binOp :: E.Exp -> Variable -> E.Exp -> E.Exp
 binOp l op r = E.App s (E.App (getSpan l) (E.Var (getSpan op) op) l) r
-  where s  = Span (startPos sl) (endPos sr) (source sl ++ " " ++ show op ++ " " ++ source sr) (defModule sl)
+  where s  = Span (startPos sl) (endPos sr) "" (defModule sl)
         sl = getSpan l
         sr = getSpan r
 
