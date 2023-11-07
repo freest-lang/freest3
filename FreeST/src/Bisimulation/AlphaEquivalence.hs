a6a80110b9f40e5afea6ab83928907b1f07c8c3e
Vasco T. Vasconcelos <vmvasconcelos@ciencias.ulisboa.pt>
2023-11-07 11:03:01 +0900

    indenting

--- FreeST/src/Bisimulation/AlphaEquivalence.hs
+++ FreeST/src/Bisimulation/AlphaEquivalence.hs
@@ -32,7 +32,8 @@ instance Equiv T.Type where
   equiv _ T.Int{} T.Int{} = True
   equiv _ T.Char{} T.Char{} = True
   equiv _ T.String{} T.String{} = True
-  equiv v (T.Arrow _ m1 t1 u1) (T.Arrow _ m2 t2 u2) = m1 == m2 && equiv v t1 t2 && equiv v u1 u2
+  equiv v (T.Arrow _ m1 t1 u1) (T.Arrow _ m2 t2 u2) =
+    m1 == m2 && equiv v t1 t2 && equiv v u1 u2
   equiv v (T.Labelled _ s1 m1) (T.Labelled _ s2 m2) =
     s1 == s2 &&
     Map.size m1 == Map.size m2 &&
@@ -43,8 +44,9 @@ instance Equiv T.Type where
   equiv v (T.Message _ p1 t1) (T.Message _ p2 t2) = p1 == p2 && equiv v t1 t2
   equiv v (T.Forall _ b1) (T.Forall _ b2) = equiv v b1 b2
   equiv v (T.Rec _ b1) (T.Rec _ b2) = equiv v b1 b2
-  equiv v (T.Var _ x1) (T.Var _ x2) = x1 == x2 ||                -- free variables
-                                      Just x2 == Map.lookup x1 v -- bound variables
+  equiv v (T.Var _ x1) (T.Var _ x2) =
+    x1 == x2 ||                -- free variables
+    Just x2 == Map.lookup x1 v -- bound variables
   equiv v (T.Dualof _ t1) (T.Dualof _ t2) = equiv v t1 t2
   equiv _ _ _ = False
 
