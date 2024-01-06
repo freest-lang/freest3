bb5f0f43fa8b6d363cc5aa90d81e6966bbd46361
Vasco T. Vasconcelos <vmvasconcelos@ciencias.ulisboa.pt>
2024-01-06 22:34:38 +0000

    polishing, commenting

--- FreeST/src/Typing/Rename.hs
+++ FreeST/src/Typing/Rename.hs
@@ -1,6 +1,6 @@
 {- |
 Module      :  Typing.Rename
-Description :  <optional short text displayed on contents page>
+Description :  Renaming expressions and types
 Copyright   :  (c) <Authors or Affiliations>
 License     :  <license>
 
@@ -8,7 +8,8 @@ Maintainer  :  <email>
 Stability   :  unstable | experimental | provisional | stable | frozen
 Portability :  portable | non-portable (<reason>)
 
-<module description starting at first column>
+Renames types and programs so that all bound variables are distinct, that is
+bear a different (natural number).
 -}
 
 {-# LANGUAGE FlexibleContexts #-}
@@ -59,87 +60,100 @@ renameFun f t = do
 
 -- Renaming the various syntactic categories
 
-type Bindings = Map.Map String Variable -- Why String and not Syntax.Base.Variable?
+type Substitution = Map.Map String Variable -- Why String and not Syntax.Base.Variable?
 
 class Rename t where
-  rename :: MonadState (FreestS a) m => Bindings -> Bindings -> t -> m t
-  -- TODO: one Bindings is enough
-  
--- Binds
+  rename :: MonadState (FreestS a) m =>
+    Substitution -> -- For type variables, σ
+    Substitution -> -- For program variables, τ
+    t -> m t
+-- Note: Type variables and program variables come from two different universes.
+-- We would like this type, Λa => λa:a -> λb:a -> a, the type of the truth value
+-- true in System F, to be well formed.
+
+-- Renaming binds
+
 instance Rename t => Rename (Bind K.Kind t) where
-  rename tbs pbs (Bind p a k t) = do
-    a' <- rename tbs pbs a
-    t' <- rename (insertVar a a' tbs) pbs t
+  rename σ τ (Bind p a k t) = do
+    a' <- rename σ τ a
+    t' <- rename (insertVar a a' σ) τ t
     return $ Bind p a' k t'
 
 instance Rename (Bind T.Type E.Exp) where
-  rename tbs pbs (Bind p x t e) = do
-    x' <- rename tbs pbs x
-    t' <- rename tbs pbs t
-    e' <- rename tbs (insertVar x x' pbs) e
+  rename σ τ (Bind p x t e) = do
+    x' <- rename σ τ x
+    t' <- rename σ τ t
+    e' <- rename σ (insertVar x x' τ) e
     return $ Bind p x' t' e'
 
--- Types
+-- Renaming types
 
 instance Rename T.Type where
   -- Functional types
-  rename tbs pbs (T.Arrow p m t u) = T.Arrow p m <$> rename tbs pbs t <*> rename tbs pbs u
-  rename tbs pbs (T.Labelled p s m) = T.Labelled p s <$> tMapM (rename tbs pbs) m
+  rename σ τ (T.Arrow p m t u) = T.Arrow p m <$> rename σ τ t <*> rename σ τ u
+  rename σ τ (T.Labelled p s m) = T.Labelled p s <$> tMapM (rename σ τ) m
   -- Session types
-  rename tbs pbs (T.Semi p t u) = T.Semi p <$> rename tbs pbs t <*> rename tbs pbs u
-  rename tbs pbs (T.Message p pol t) = T.Message p pol <$> rename tbs pbs t
+  rename σ τ (T.Semi p t u) = T.Semi p <$> rename σ τ t <*> rename σ τ u
+  rename σ τ (T.Message p pol t) = T.Message p pol <$> rename σ τ t
   -- Polymorphism and recursive types
-  rename tbs pbs (T.Forall p b) = T.Forall p <$> rename tbs pbs b
-  rename tbs pbs (T.Rec p b) = T.Rec p <$> rename tbs pbs b
-  rename tbs _ (T.Var p a) = return $ T.Var p (findWithDefaultVar a tbs)
+  rename σ τ (T.Forall p b) = T.Forall p <$> rename σ τ b
+  rename σ τ (T.Rec p b) = T.Rec p <$> rename σ τ b
+  rename σ _ (T.Var p a) = return $ T.Var p (findWithDefaultVar a σ)
   -- Type operators
-  rename tbs pbs (T.Dualof p t@T.Var{}) = T.Dualof p <$> rename tbs pbs t
+  rename σ τ (T.Dualof p t@T.Var{}) = T.Dualof p <$> rename σ τ t
   rename _ _ t@T.Dualof{} = internalError "Typing.Rename.rename" t
   -- Int, Float, Char, String, Skip, End
   rename _ _ t = return t
 
--- Expressions
+renameType :: T.Type -> T.Type
+renameType = head . renameTypes . (: [])
+
+renameTypes :: [T.Type] -> [T.Type]
+renameTypes ts =
+  evalState (mapM (rename Map.empty Map.empty) ts) initialS
+
+-- Renaming expressions
 
 instance Rename E.Exp where
   -- Variable
-  rename _ pbs (E.Var p x) = return $ E.Var p (findWithDefaultVar x pbs)
+  rename _ τ (E.Var p x) = return $ E.Var p (findWithDefaultVar x τ)
   -- Abstraction intro and elim
-  rename tbs pbs (E.Abs p m b) = E.Abs p m <$> rename tbs pbs b
-  rename tbs pbs (E.App p e1 e2) = E.App p <$> rename tbs pbs e1 <*> rename tbs pbs e2
+  rename σ τ (E.Abs p m b) = E.Abs p m <$> rename σ τ b
+  rename σ τ (E.App p e1 e2) = E.App p <$> rename σ τ e1 <*> rename σ τ e2
   -- Pair intro and elim
-  rename tbs pbs (E.Pair p e1 e2) = E.Pair p <$> rename tbs pbs e1 <*> rename tbs pbs e2
-  rename tbs pbs (E.BinLet p x y e1 e2) = do
-    x'  <- rename tbs pbs x
-    y'  <- rename tbs pbs y
-    e1' <- rename tbs pbs e1
-    e2' <- rename tbs (insertVar y y' (insertVar x x' pbs)) e2
+  rename σ τ (E.Pair p e1 e2) = E.Pair p <$> rename σ τ e1 <*> rename σ τ e2
+  rename σ τ (E.BinLet p x y e1 e2) = do
+    x'  <- rename σ τ x
+    y'  <- rename σ τ y
+    e1' <- rename σ τ e1
+    e2' <- rename σ (insertVar y y' (insertVar x x' τ)) e2
     return $ E.BinLet p x' y' e1' e2'
   -- Datatype elim
-  rename tbs pbs (E.Case p e fm) =
-    E.Case p <$> rename tbs pbs e <*> tMapM (renameField tbs pbs) fm
+  rename σ τ (E.Case p e fm) =
+    E.Case p <$> rename σ τ e <*> tMapM (renameField σ τ) fm
   -- Type application & TypeAbs
-  rename tbs pbs (E.TypeAbs p b) = E.TypeAbs p <$> rename tbs pbs b
-  rename tbs pbs (E.TypeApp p e t) =
-    E.TypeApp p <$> rename tbs pbs e <*> rename tbs pbs t
+  rename σ τ (E.TypeAbs p b) = E.TypeAbs p <$> rename σ τ b
+  rename σ τ (E.TypeApp p e t) =
+    E.TypeApp p <$> rename σ τ e <*> rename σ τ t
   -- Let
-  rename tbs pbs (E.UnLet p x e1 e2) = do
-    x'  <- rename tbs pbs x
-    e1' <- rename tbs pbs e1
-    e2' <- rename tbs (insertVar x x' pbs) e2
+  rename σ τ (E.UnLet p x e1 e2) = do
+    x'  <- rename σ τ x
+    e1' <- rename σ τ e1
+    e2' <- rename σ (insertVar x x' τ) e2
     return $ E.UnLet p x' e1' e2'
   -- Otherwise: Unit, Int, Float, Char, String
   rename _ _ e = return e
 
-renameField :: MonadState (FreestS a) m => Bindings -> Bindings -> ([Variable], E.Exp) -> m ([Variable], E.Exp)
-renameField tbs pbs (xs, e) = do
-  xs' <- mapM (rename tbs pbs) xs
-  e'  <- rename tbs (insertProgVars xs') e
+renameField :: MonadState (FreestS a) m => Substitution -> Substitution -> ([Variable], E.Exp) -> m ([Variable], E.Exp)
+renameField σ τ (xs, e) = do
+  xs' <- mapM (rename σ τ) xs
+  e'  <- rename σ (insertProgVars xs') e
   return (xs', e')
  where
-  insertProgVars :: [Variable] -> Bindings
-  insertProgVars xs' = foldr (uncurry insertVar) pbs (zip xs xs')
+  insertProgVars :: [Variable] -> Substitution
+  insertProgVars xs' = foldr (uncurry insertVar) τ (zip xs xs')
 
--- Rename variables
+-- Renaming variables
 
 instance Rename Variable where
   rename _ _ = renameVar
@@ -149,20 +163,13 @@ renameVar x = do
   n <- getNextIndex
   return $ mkNewVar n x
 
-insertVar :: Variable -> Variable -> Bindings -> Bindings
+-- Managing variables
+
+insertVar :: Variable -> Variable -> Substitution -> Substitution
 insertVar x = Map.insert (intern x)
 
-findWithDefaultVar :: Variable -> Bindings -> Variable
+findWithDefaultVar :: Variable -> Substitution -> Variable
 findWithDefaultVar x = Map.findWithDefault x (intern x)
-
--- Rename a type
-renameType :: T.Type -> T.Type
-renameType = head . renameTypes . (: [])
-
--- Rename a list of types
-renameTypes :: [T.Type] -> [T.Type]
-renameTypes ts =
-  evalState (mapM (rename Map.empty Map.empty) ts) initialS
 
 -- Substitution and unfold, the renamed versions
 
