653bb2c423ed9f32072b8c55b3851d3967b115b2
Vasco T. Vasconcelos <vmvasconcelos@ciencias.ulisboa.pt>
2024-01-07 00:21:24 +0000

    Annotating module Kinding.Contractive

--- FreeST/src/Kinding/Contractive.hs
+++ FreeST/src/Kinding/Contractive.hs
@@ -1,6 +1,7 @@
 {-|
 Module      :  Kinding.Contractive
-Description :  <optional short text displayed on contents page>
+Description :  Is a given type contractive on a recursion variable,
+               given a set of polymorphic variables?
 Copyright   :  (c) <Authors or Affiliations>
 License     :  <license>
 
@@ -8,7 +9,14 @@ Maintainer  :  <email>
 Stability   :  unstable | experimental | provisional | stable | frozen
 Portability :  portable | non-portable (<reason>)
 
-<module description starting at first column>
+Rules from
+
+Bernardo Almeida, Andreia Mordido, Peter Thiemann, Vasco T. Vasconcelos:
+Polymorphic lambda calculus with context-free session types.
+Inf. Comput. 289(Part): 104948 (2022)
+
+except that we omit rule C-TAbs given that forall does exhibit a type
+constructor. We want type (rec a . forall b: 1S . a) to be well formed.
 -}
 
 module Kinding.Contractive
@@ -24,9 +32,8 @@ import qualified Data.Set as Set
 
 contractive :: K.PolyVars -> Variable -> T.Type -> Bool
 contractive s a (T.Semi _ t u)
-  | terminated t                         = contractive s a u
-  | otherwise                            = contractive s a t
-contractive s a (T.Rec _ (Bind _ _ _ t)) = contractive s a t
-contractive s a (T.Var _ b)              = a /= b && b `Set.notMember` s
-contractive _ _ _                        = True
-
+  | terminated t                         = contractive s a u             -- C-Seq1
+  | otherwise                            = contractive s a t             -- C-Seq2
+contractive s a (T.Rec _ (Bind _ _ _ t)) = contractive s a t             -- C-Rec
+contractive s a (T.Var _ b)              = b /= a && b `Set.notMember` s -- C-Var
+contractive _ _ _                        = True                          -- C-Other
