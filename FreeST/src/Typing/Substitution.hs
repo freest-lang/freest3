9d3e9bc308da3a4a621917dadc4089b4e63c13ff
Bernardo Almeida <balmeida@lasige.di.fc.ul.pt>
2024-03-21 17:10:45 +0000

    polishing

--- FreeST/src/Typing/Substitution.hs
+++ FreeST/src/Typing/Substitution.hs
@@ -32,7 +32,7 @@ import           Elaboration.Duality
 import qualified Data.Set as Set
 
 
--- [t/x]u, substitute t for for every occurrence of x in u
+-- [t/a]u, substitute t for for every occurrence of a in u
 
 class Subs a where
   subs :: T.Type -> Variable -> a -> a
@@ -42,23 +42,23 @@ class Subs a where
 
 instance Subs  T.Type where
   -- Labelled
-  subs t x (T.Labelled s k m) = T.Labelled s k (Map.map (subs t x) m)
+  subs t a (T.Labelled s k m) = T.Labelled s k (Map.map (subs t a) m)
   -- Functional types
-  subs t x (T.Message s pol t1) = T.Message s pol (subs t x t1)
-  subs t x (T.Arrow s m t1 t2) = T.Arrow s m (subs t x t1) (subs t x t2)
+  subs t a (T.Message s pol t1) = T.Message s pol (subs t a t1)
+  subs t a (T.Arrow s m t1 t2) = T.Arrow s m (subs t a t1) (subs t a t2)
   -- Session types
-  subs t x (T.Semi s t1 t2) = T.Semi s (subs t x t1) (subs t x t2)
+  subs t a (T.Semi s t1 t2) = T.Semi s (subs t a t1) (subs t a t2)
   -- Polymorphism and recursion
-  subs t x (T.Rec s b) = T.Rec s (subs t x b)
-  subs t x (T.Forall s b) = T.Forall s (subs t x b)
-  subs t x u@(T.Var _ y)
-    | y == x    = t
+  subs t a (T.Rec s b) = T.Rec s (subs t a b)
+  subs t a (T.Forall s b) = T.Forall s (subs t a b)
+  subs t a u@(T.Var _ y)
+    | y == a    = t
     | otherwise = u
-  subs (T.Var _ t) x u@(T.Dualof s (T.Var p' y))
-    | y == x    = T.Dualof s $ T.Var p' t
+  subs (T.Var _ t) a u@(T.Dualof s1 (T.Var s2 b))
+    | a == b    = T.Dualof s1 $ T.Var s2 t
     | otherwise = u
-  subs t x u@(T.Dualof s (T.Var p' y))
-    | y == x    = dualof t
+  subs t a u@(T.Dualof s1 (T.Var s2 b))
+    | a == b    = dualof t
     | otherwise = u
   subs _ _ t = t
   -- Can't issue this error because we use
@@ -66,35 +66,35 @@ instance Subs  T.Type where
   --  subs _ _ t@T.Dualof{} = internalError "Typing.Substitution.subs" t
 
 instance (Subs t) => Subs (Bind k t) where
-  subs t x (Bind p y k u) = Bind p y k (subs t x u)
+  subs t a (Bind p y k u) = Bind p y k (subs t a u)
 
--- CoVar subs, [t/co-x]u
+-- [t/co-a]u, substitute t for for every occurrence of covariable a in u
 
-class Cosubs t where
-  cosubs :: T.Type -> Variable -> t -> t
+class Cosubs a where
+  cosubs :: T.Type -> Variable -> a -> a
 
 instance Cosubs T.Type where
   -- Functional types
-  cosubs t x (T.Message s pol t1) = T.Message s pol (cosubs t x t1)
-  cosubs t x (T.Arrow s m t1 t2 ) = T.Arrow s m (cosubs t x t1) (cosubs t x t2)
+  cosubs t a (T.Message s pol t1) = T.Message s pol (cosubs t a t1)
+  cosubs t a (T.Arrow s m t1 t2 ) = T.Arrow s m (cosubs t a t1) (cosubs t a t2)
   -- Session types
-  cosubs t x (T.Semi s t1 t2) = T.Semi s (cosubs t x t1) (cosubs t x t2)
-  cosubs t x (T.Labelled s k m) = T.Labelled s k (Map.map (cosubs t x) m)
+  cosubs t a (T.Semi s t1 t2) = T.Semi s (cosubs t a t1) (cosubs t a t2)
+  cosubs t a (T.Labelled s k m) = T.Labelled s k (Map.map (cosubs t a) m)
     -- Polymorphism and recursion
-  cosubs t x (T.Rec s b) = T.Rec s (cosubs t x b)
-  cosubs t x (T.Forall s b) = T.Forall s (cosubs t x b)
-  cosubs t x u@(T.Dualof _ (T.Var _ y))
-    | y == x = t
+  cosubs t a (T.Rec s b) = T.Rec s (cosubs t a b)
+  cosubs t a (T.Forall s b) = T.Forall s (cosubs t a b)
+  cosubs t a u@(T.Dualof _ (T.Var _ b))
+    | a == b    = t
     | otherwise = u
   cosubs _ _ t = t
 
 instance Cosubs t => Cosubs (Bind K.Kind t) where
-  cosubs t x (Bind p y k u) = Bind p y k (cosubs t x u)
+  cosubs t a (Bind p y k u) = Bind p y k (cosubs t a u)
 
 
 -- Unfold a recursive type (one step only)
 unfold :: T.Type -> T.Type
-unfold t@(T.Rec _ (Bind _ x _ u)) = subs t x u
+unfold t@(T.Rec _ (Bind _ a _ u)) = subs t a u
 unfold t = internalError "Typing.Substitution.unfold" t
 
 -- The set of free type variables in a type
@@ -104,8 +104,8 @@ free (T.Labelled _ _ m) =
   Map.foldr (\t acc -> free t `Set.union` acc) Set.empty m
 free (T.Message _ _ t) = free t 
 free (T.Semi _ t u) = free t `Set.union` free u
-free (T.Rec _ (Bind _ x _ t)) = Set.delete x (free t)
-free (T.Forall _ (Bind _ x _ t)) = Set.delete x (free t)
+free (T.Rec _ (Bind _ a _ t)) = Set.delete a (free t)
+free (T.Forall _ (Bind _ a _ t)) = Set.delete a (free t)
 free (T.Var _ x) = Set.singleton x
 free (T.Dualof _ x) = free x
 free _ = Set.empty 
