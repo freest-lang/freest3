7c1c9c5272812f33101e613fabd79a06800788eb
Miguel Roldão <miguelroldao9@gmail.com>
2024-05-07 18:37:22 +0100

    clean up + tests

--- FreeST/src/Bisimulation/Minimal.hs
+++ FreeST/src/Bisimulation/Minimal.hs
@@ -18,6 +18,8 @@ in `T`. This facilitates translation into grammars.
 module Bisimulation.Minimal
   ( minimal
   , first
+  , minimalF
+  , firstF
   )
 where
 
@@ -34,35 +36,37 @@ import qualified Data.Set            as Set
 
 import           Debug.Trace
 
-minimal = minimalS cleanFresh
+minimal :: Type -> Type
+minimal t = trace ("# N[" ++ show t ++ "] => " ++ show t') t'
+  where t' = minimalF cleanFresh t
 
-minimalS :: [Int] -> Type -> Type
--- f: set of available fresh variables
+minimalF :: [Int] -> Type -> Type
+-- f: list of possible fresh variables
   -- Functional Types
-minimalS f (Arrow s m t u) = Arrow s m (minimalS f t) (minimalS f u)
-minimalS f (Labelled s k m) = Labelled s k (Map.map (minimalS f) m)
+minimalF f (Arrow s m t u) = Arrow s m (minimalF f t) (minimalF f u)
+minimalF f (Labelled s k m) = Labelled s k (Map.map (minimalF f) m)
 -- session types
-minimalS f (Message s p t) = Message s p (minimalS f t)
-minimalS f (Semi s t u) = Semi s (minimalS f' t) (minimalS f u)
+minimalF f (Message s p t) = Message s p (minimalF f t)
+minimalF f (Semi s t u) = Semi s (minimalF f' t) (minimalF f u)
   where f' = filter (`notElem` freei u) f
   -- Polymorphism and recursive types
-minimalS f t@(Forall s1 (Bind s2 a k u)) =
-  --trace "# inside forall" (Forall s1 (Bind s2 b k (minimalS f (subs vb a u))))
-  Forall s1 (Bind s2 b k (minimalS f (subs vb a u)))
-    where ai = firstS f u
-          --b = mkNewVar ai a  -- to keep original var meta
-          b = Variable (getSpan a) ("#" ++ extern a) ai
+minimalF f t@(Forall s1 (Bind s2 a k u)) =
+  Forall s1 (Bind s2 b k (minimalF f (subs vb a u)))
+    where ai = firstF f u
+          --b = Variable (getSpan a) ("#" ++ extern a) ai
+          b = Variable (getSpan a) ("#" ++ show ai) ai
+          -- TODO: perguntar prof. vasco
           vb = Var (getSpan b) b
-minimalS f (Rec s1 (Bind s2 a k t))
-  | a `isFreeIn` t = Rec s1 (Bind s2 a k (minimalS f t))
+minimalF f (Rec s1 (Bind s2 a k t))
+  | a `isFreeIn` t = Rec s1 (Bind s2 a k (minimalF f t))
   -- Required by the current translation to grammar. Otherwise:
   -- uncaught exception: PatternMatchFail
   --  src/Bisimulation/TypeToGrammar.hs:130:3-39: Non-exhaustive patterns in z : zs  
-  | otherwise = minimalS f t
+  | otherwise = minimalF f t
   -- Type operators
-minimalS f (Dualof s t) = Dualof s (minimalS f t)
+minimalF f (Dualof s t) = Dualof s (minimalF f t)
   -- Int, Float, Char, String, Skip, End, Var
-minimalS _ t = t
+minimalF _ t = t
 
 
 -- precondition: dualof is only applied to T.Var at this point
@@ -76,30 +80,18 @@ minimalS _ t = t
 --whnf _                     = True
 
 
-
 -- OBS: fresh variables can't be implemented with sets, as Data.Set doesn't support infinite sets. It hangs.
-
---cleanFresh :: Set.Set Variable
---cleanFresh = Set.fromAscList $ map (\i -> Variable defaultSpan ("#" ++ show i) i) [1..]
-
---first :: Type -> Variable
---first = firstS cleanFresh
-
---firstS :: Set.Set Variable -> Type -> Variable
---firstS fresh t = head $ filter (`Set.notMember` free t) (Set.toList fresh)
 
 cleanFresh = [1..]
 
-first = firstS cleanFresh
+first = firstF cleanFresh
 
 -- assumes list is ordered asc.
-firstS :: [Int] -> Type -> Int
-firstS fresh t = trace ("# Chosen var: " ++ show f ++ ". Type: \"" ++ show t ++ "\"") f
+firstF :: [Int] -> Type -> Int
+firstF fresh t = f
   where f = head $ filter (`notElem` freei t) fresh
 
 freei :: Type -> [Int]
---freei t = trace ("# Free vars in \"" ++ show t ++ "\": " ++ show fv) fv
-freei t = fv
-  where fv = Set.toList $ Set.map (\(Variable _ _ n) -> n) $ Set.filter (\(Variable _ id _) -> head id == '#')(free t)
+freei t = Set.toList $ Set.map (\(Variable _ _ n) -> n) $ Set.filter (\(Variable _ id _) -> head id == '#') (free t)
 
 
