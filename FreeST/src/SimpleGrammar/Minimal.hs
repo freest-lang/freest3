d7630c7f7b5461462dc76a17792383942c539e3a
Vasco T. Vasconcelos <vmvasconcelos@ciencias.ulisboa.pt>
2024-07-25 12:34:48 +0100

    fixing the definition of minimal renaming - quick check is good now

--- FreeST/src/SimpleGrammar/Minimal.hs
+++ FreeST/src/SimpleGrammar/Minimal.hs
@@ -29,34 +29,44 @@ import qualified Data.Map.Strict     as Map
 import qualified Data.Set as Set
 
 minimal :: Type -> Type
+minimal = minimal' Set.empty
+
+minimal' :: Set.Set Variable -> Type -> Type
   -- Functional Types
-minimal (Arrow s m t u) = Arrow s m (minimal t) (minimal u)
-minimal (Labelled s k m) = Labelled s k (Map.map minimal m)
+minimal' fv (Arrow s m t u) = Arrow s m (minimal' (fv `Set.union` free u) t) (minimal' fv u)
+  -- Functional and Session Types
+minimal' fv (Labelled s k m) = Labelled s k (Map.map (minimal' fv) m)
   -- Session Types
-minimal (Semi s t u) = Semi s (minimal t) (minimal u)
-minimal (Message s p t) = Message s p (minimal t)
+minimal' fv (Semi s t u) = Semi s (minimal' (fv `Set.union` free u) t) (minimal' fv u)
+minimal' fv (Message s p t) = Message s p (minimal' fv t)
   -- Polymorphism and recursive types
-minimal t@(Forall s1 (Bind s2 a k u)) =
-  Forall s1 (Bind s2 b k (minimal (subs vb a u)))
-    where b = mkNewVar (first t) a
+minimal' fv t@(Forall s1 (Bind s2 a k u)) =
+  Forall s1 (Bind s2 b k (minimal' fv (subs vb a u)))
+    where b = first fv t -- mkNewVar (first t) a
           vb = Var (getSpan b) b
-minimal (Rec s1 (Bind s2 a k t))
-  | a `isFreeIn` t = Rec s1 (Bind s2 a k (minimal t))
+minimal' fv (Rec s1 (Bind s2 a k t))
+  | a `isFreeIn` t = Rec s1 (Bind s2 a k (minimal' fv t))
   -- Required by the current translation to grammar. Otherwise:
   -- uncaught exception: PatternMatchFail
   --  src/Bisimulation/TypeToGrammar.hs:130:3-39: Non-exhaustive patterns in z : zs  
-  | otherwise = minimal t
+  | otherwise = minimal' fv t
   -- Type operators
-minimal (Dualof s t) = Dualof s (minimal t)
+minimal' fv (Dualof s t) = Dualof s (minimal' fv t)
   -- Int, Float, Char, String, Skip, End, Var
-minimal t = t
+minimal' _ t = t
 
-first :: Type -> Int
-first t = first' 0 (Set.toList (Set.map (\(Variable _ _ n) -> n) (free t)))
-  -- TODO: avoid converting to list
+first :: Set.Set Variable -> Type -> Variable
+first fv t = head $ filter (`Set.notMember` (fv `Set.union` free t)) freshVars
   where
-    first' :: Int -> [Int] -> Int
-    first' n [] = n
-    first' n (m:ms)
-      | n < m     = n
-      | otherwise = first' (n + 1) ms
+    -- ["#1", "#2", ...]
+    freshVars = map (\i -> mkVar defaultSpan $ "#" ++ show i) [1..]
+
+-- first :: Type -> Int
+-- first t = first' 0 (Set.toList (Set.map (\(Variable _ _ n) -> n) (free t)))
+--   -- TODO: avoid converting to list
+--   where
+--     first' :: Int -> [Int] -> Int
+--     first' n [] = n
+--     first' n (m:ms)
+--       | n < m     = n
+--       | otherwise = first' (n + 1) ms
