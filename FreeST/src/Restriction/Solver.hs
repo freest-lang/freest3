6e397adc46f20ba45de3e6e3585303a436013761
Luís Santos <fc56341@alunos.fc.ul.pt>
2025-10-17 16:57:45 +0100

    removed unnecessary pragma

--- FreeST/src/Restriction/Solver.hs
+++ FreeST/src/Restriction/Solver.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 
 module Restriction.Solver (solveInequalities) where
